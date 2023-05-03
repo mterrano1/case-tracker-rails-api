eb834c64418a2c47d349f783466dcec73910d3f6
Christopher Terrano <mterrano1@yahoo.com>
2023-05-03 12:40:21 -0500

    add error messages to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    private
+
+    def render_unprocessable_entity_response(e)
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
