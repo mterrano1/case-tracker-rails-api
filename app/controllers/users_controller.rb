b865a01d0614391ff3efd12c2bcbde7487496995
Christopher Terrano <mterrano1@yahoo.com>
2023-05-03 12:39:25 -0500

    add create method and user_params to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :username, :password, :password_confirmation)
+    end
 end
