1f899b37850bbf7dd9bae067101d093557ea89f4
Christopher Terrano <mterrano1@yahoo.com>
2023-05-03 12:36:32 -0500

    add validations and bcrypt password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    # Uses bcrypt to store a secure password in the database
+    has_secure_password
+
+    validates :first_name, :last_name, :username, presence: true
+    validates :username, uniqueness: { case_sensitive: false }
 end
